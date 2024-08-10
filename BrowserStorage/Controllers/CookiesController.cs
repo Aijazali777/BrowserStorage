3308bb18d5a1d7e0a4cb0cdac4326b9dadfdcfe5
AliAijaz <aaijaz@trafix.com>
2024-08-10 12:48:09 +0500

    Added project files

---
+++ BrowserStorage/Controllers/CookiesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrowserStorage.Controllers
+{
+    public class CookiesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var key = "PhoneNumber";
+            ViewBag.PhoneNumber = Request.Cookies[key];
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string values)
+        {
+            var data = JsonConvert.DeserializeObject<IDictionary>(values);
+            string key = Convert.ToString(data["Key"]);
+            string value = Convert.ToString(data["Value"]);
+            CookieOptions options = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(2)
+            };
+            Response.Cookies.Append(key, value, options);
+            return View();
+        }
+    }
+}
---
+++ BrowserStorage/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrowserStorage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
